bb2fd9a0cf5198abc5cd9f58cb39b6902b570147
Alex <temper8@gmail.com>
2023-04-11 23:49:09 +0300

    added Matmul_test project

---
+++ Matmul_test/Matmul_test.f90
@@ -0,0 +1,25 @@
+!  Matmul_test.f90 
+!
+!  FUNCTIONS:
+!  Matmul_test - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Matmul_test
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Matmul_test
+
+    implicit none
+
+    ! Variables
+
+    ! Body of Matmul_test
+    print *, 'Hello Matmul'
+    pause
+    end program Matmul_test
+
