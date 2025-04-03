b2c7dc60d01615f307e73af91bcf808b1ead7c45
Alex <temper8@gmail.com>
2025-04-03 09:09:18 +0300

    create MatrixInversionTest

---
+++ MatrixInversionTest/MatrixInversionTest.f90
@@ -0,0 +1,25 @@
+!  MatrixInversionTest.f90 
+!
+!  FUNCTIONS:
+!  MatrixInversionTest - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: MatrixInversionTest
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program MatrixInversionTest
+
+    implicit none
+
+    ! Variables
+
+    ! Body of MatrixInversionTest
+    print *, 'Hello World'
+
+    end program MatrixInversionTest
+
