55662ca1e708cc04e88484ef0634bf6d5178a75a
Alex <temper8@gmail.com>
2025-04-03 09:00:11 +0300

    add matmul_MKL

--- Matmul_test/Matmul_test.f90
+++ Matmul_test/Matmul_test.f90
@@ -16,55 +16,88 @@
     use time_module
     use matmul_module
     implicit none
-     integer nn
-     real(8), dimension(5000,5000) :: a, b, c
+     integer nn, i, istat
+     character(len=70) errmes_string
+     real(8), allocatable, dimension(:,:) :: a, b, c
      real   :: all_time1, all_time2
      real   :: s_time
      real   :: time21, time22
      real   ::  cpu_t1,cpu_t2 
-     real   ::  cpu_dt1, cpu_dt2, cpu_dt3 
-     real   ::  sys_dt1, sys_dt2, sys_dt3 
+     real   ::  cpu_dt(5)
+     real   ::  sys_dt(5)
      
      all_time1 = sys_time()
      print *, 'Hello, matmul'
      
      call print_compiler_info
      
-     print *, "         n", "      init","         no-parallel", "     parallel"
+     print *, "         n", "         no-parallel", "     parallel  ",  "       MKL     ",  "     MatMul"
      
-     do nn = 500, 2500, 100
+     do nn = 500, 1500, 100
+         allocate(a(nn,nn), b(nn,nn),c(nn,nn), stat=istat, errmsg=errmes_string )
+         if ( istat /= 0 ) then
+            write ( *,* )  errmes_string,' : istat =',istat
+         endif
          s_time = sys_time()
          call cpu_time(cpu_t1) 
          CALL RANDOM_NUMBER(a)
          CALL RANDOM_NUMBER(b)    
          call cpu_time(cpu_t2)
-         sys_dt1 = sys_time() - s_time
-         cpu_dt1 = cpu_t2-cpu_t1
+         sys_dt(1) = sys_time() - s_time
+         cpu_dt(1) = cpu_t2-cpu_t1
          !print *, sum(c) 
 
          s_time = sys_time()
          call cpu_time(cpu_t1) 
-         call matmul_no_parallel(a,b,c,nn)
+         do i = 1,10
+            !c = MatMul(a,b)
+            call matmul_no_parallel(a,b,c,nn)
+            !call matmul_MKL(a,b,c,nn)
+         enddo
+
          call cpu_time(cpu_t2)
-         sys_dt2 = sys_time() - s_time
-         cpu_dt2 = cpu_t2-cpu_t1
+         sys_dt(2) = sys_time() - s_time
+         cpu_dt(2) = cpu_t2-cpu_t1
          !print *, sum(c)     
 
          s_time = sys_time()
          call cpu_time(cpu_t1) 
-         call matmul_parallel(a,b,c,nn)
+         do i = 1,10
+            call matmul_parallel(a,b,c,nn)
+         enddo
          call cpu_time(cpu_t2)
-         sys_dt3 = sys_time() - s_time
-         cpu_dt3 = cpu_t2-cpu_t1
+         sys_dt(3) = sys_time() - s_time
+         cpu_dt(3) = cpu_t2 - cpu_t1
          !print *, sum(c)
+         s_time = sys_time()
+         call cpu_time(cpu_t1) 
+         do i = 1,10
+            !c = matmul_MKL_f(a,b)
+            call matmul_MKL(a,b,c)
+            !call matmul_MKL(a,b,c, nn)
+         enddo
+         call cpu_time(cpu_t2)
+         sys_dt(4) = sys_time() - s_time
+         cpu_dt(4) = cpu_t2 - cpu_t1
 
-         print *, nn, cpu_dt1, cpu_dt2, cpu_dt3
-         print *,  0, sys_dt1, sys_dt2, sys_dt3
+         s_time = sys_time()
+         call cpu_time(cpu_t1) 
+         do i = 1,10
+            c = MatMul(a,b)
+         enddo
+         call cpu_time(cpu_t2)
+         sys_dt(5) = sys_time() - s_time
+         cpu_dt(5) = cpu_t2 - cpu_t1         
+         
+         print *, nn, "---------------"
+         print *, ' cpu time   ', cpu_dt(2:5)
+         print *, ' sys time   ', sys_dt(2:5)
+         deallocate(a, b, c)
      end do
      all_time2 = sys_time()
      print *,"all time =", all_time2 - all_time1
 
-    !pause
+    pause
     
     contains
         subroutine print_compiler_info
--- Matmul_test/matmul_module.f90
+++ Matmul_test/matmul_module.f90
@@ -19,7 +19,7 @@ module matmul_module
     subroutine matmul_parallel(a,b,c,n)
 	    real(8) a(n,n),b(n,n),c(n,n)
 	    c=0.d0
-	    !!!DIR$ PARALLEL  ALWAYS
+	    !DIR$ PARALLEL  ALWAYS
 	    do i=1,n         ! Outer loop is parallelized.
 	        do j=1,n      ! inner loops are interchanged
 		        do k=1,n   ! new inner loop is vectorized 
@@ -28,4 +28,21 @@ module matmul_module
 	        enddo
 	    enddo
     end
+    !subroutine matmul_MKL(a,b,c, n)
+    subroutine matmul_MKL(a,b,c)
+        integer n
+	    real(8) a(:,:),b(:,:),c(:,:)
+	    c=0.d0
+        n = size(a,1)
+        call dgemm('N', 'N', n, n, n, 1.0d0, a, n, b, n, 0.0d0, c, n)
+    end    
+    
+    function matmul_MKL_f(a,b) result(c)
+        integer n
+	    real(8), allocatable :: a(:,:),b(:,:), c(:,:)
+        n = size(a,1)
+        allocate(c(n,n))
+	    c=0.d0
+        call dgemm('N', 'N', n, n, n, 1.0d0, a, n, b, n, 0.0d0, c, n)
+    end    
 end module matmul_module
